78b58a3b9d7409ff7b86bce7fa1ae033a13adfb5
Harrison Gottlieb <hlgott1593@gmail.com>
2021-01-09 20:34:39 -0800

    adding star backdrop

---
+++ Assets/Scripts/FixedScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedScroll : MonoBehaviour
+{
+    public float theScrollSpeed = 0.025f;
+	Transform theCamera;
+
+	void Start () 
+	{
+	}
+	
+	void Update ()
+	{
+		transform.position = new Vector3 ( transform.position.x, transform.position.y + theScrollSpeed, 0);
+	}
+}
