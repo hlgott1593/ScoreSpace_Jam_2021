ece4147ccfb1482f0cb8db6673e0d8efcdcddda9
Harrison Gottlieb <hlgott1593@gmail.com>
2021-01-10 21:27:57 -0800

    tweak astroids

--- Assets/Scripts/AmmoChanger.cs
+++ Assets/Scripts/AmmoChanger.cs
@@ -5,9 +5,5 @@ using UnityEngine;
 [RequireComponent(typeof(Cannon))]
 public class AmmoChanger : MonoBehaviour {
     [SerializeField] private Cannon cannon;
-
     [SerializeField] public List<GameObject> bombs;
-    
-    
-
 }
